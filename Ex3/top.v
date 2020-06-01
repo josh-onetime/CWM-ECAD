9a09c7dd874051c3710262b6adcfbb5c2bde4b9c
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-01 15:48:39 +0000

    A complete solution to Ex1

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,13 +1,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 
-// Student Name:
-// Date: 
+// Student Name: Joshua Gei
+// Date: 1/6/2020
 //
-//  Description: In this exercise, you need to design an up / down counter, where 
-//  if the rst=1, the counter should be set to zero. If enable=0, the value
-//  should stay constant. If direction=1, the counter should count up every
-//  clock cycle, otherwise it should count down.
-//  Wrap-around values are allowed.
+//  Description: In this exercise, you need to design an up / down counter, where: 
+//  - If the rst=1, the counter should be set to zero. 
+//  - If enable=0, the value should stay constant. 
+//  - If direction=1, the counter should count up every clock cycle, otherwise it should count down.
+//  - Wrap-around values are allowed.
 //
 //  inputs:
 //           clk, rst, enable, direction
@@ -19,12 +19,29 @@
 `timescale 1ns / 100ps
 
 module counter(
-    //Todo: add ports 
+	clk		,	// clock input
+	rst		,	// reset input
+	enable		,	// enable input
+	direction	,	// direction control for counter
+	counter_out		// output of the counter
+	);
 
-    );
+// Ports
+    	output [7:0] counter_out;	// output port
+	input direction,clk,rst,enable;	// input ports
                     
-    //Todo: add registers and wires, if needed
+//Register
+	reg [7:0] counter_out;
 
-    //Todo: add user logic
-      
+//Logic - put in enable
+      	always @(posedge clk)
+	if (rst) begin 
+  	counter_out <= 8'b0 ; 	//resets counter to zero if rst = 1
+	end else if (enable==0) begin 
+	counter_out <= counter_out; //if enable=0 keep constant	
+	end else if (direction) begin
+  	counter_out <= counter_out + 1;	//if direction=1 count up
+	end else begin
+  	counter_out <= counter_out - 1; //if direction=0 count down
+	end
 endmodule
