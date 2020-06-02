5f2627e77bcfea3a4061d6ab058564e2dbbca3cc
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-02 08:30:56 +0000

    A complete (rigorous) testbench to Ex3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,52 +1,4 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Exercise #3 
-// Student Name: Joshua Gei
-// Date: 1/6/2020
-//
-//  Description: In this exercise, you need to design an up / down counter, where: 
-//  - If the rst=1, the counter should be set to zero. 
-//  - If enable=0, the value should stay constant. 
-//  - If direction=1, the counter should count up every clock cycle, otherwise it should count down.
-//  - Wrap-around values are allowed.
-//
-//  inputs:
-//           clk, rst, enable, direction
-//
-//  outputs:
-//           counter_out[7:0]
-//////////////////////////////////////////////////////////////////////////////////
-
-`timescale 1ns / 100ps
-
-module counter(
-	clk		,	// clock input
-	rst		,	// reset input
-	enable		,	// enable input
-	direction	,	// direction control for counter
-	counter_out		// output of the counter
-	);
-
-// Ports
-    	output [7:0] counter_out;	// output port
-	input direction,clk,rst,enable;	// input ports
-                    
-//Register
-	reg [7:0] counter_out;
-
-//Logic - put in enable
-      	always @(posedge clk)
-	if (rst) begin 
-  	counter_out <= 8'b0 ; 	//resets counter to zero if rst = 1
-	end else if (enable==0) begin 
-	counter_out <= counter_out; //if enable=0 keep constant	
-	end else if (direction) begin
-  	counter_out <= counter_out + 1;	//if direction=1 count up
-	end else begin
-  	counter_out <= counter_out - 1; //if direction=0 count down
-	end
-endmodule
-
-//////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
 // Student Name: Joshua Gei
 // Date: 1/6/2020
@@ -89,16 +41,16 @@ end
 
 //User logic: Test in order: 1) direction: up and down, 2) reset, 3)enable
 
-// 1. Initialize testbench variables to 0 at start of simulation
+// 1. Initialize testbench variables at start of simulation
 initial begin
        err=0;
        rst=0;
        direction=1;     //begin with an upwards direction
-       enable=1;
+       enable=1;        //counter is enabled
        
     // 1. Test for direction  
     #CLK_PERIOD
-    if (counter_out==8'b0)
+    if (counter_out==8'b00000000)
     begin
         $display("***TEST FAILED!");    //this is not a rigorous solution. will try to improve my code.
         err=1;
@@ -107,7 +59,7 @@ initial begin
     // 2. Test for reset 
     rst = 1; 
     #CLK_PERIOD
-    if (counter_out==8'b0)
+    if (counter_out!=8'b0)
     begin
         $display("***TEST FAILED!");    
         err=1;
@@ -123,17 +75,30 @@ initial begin
         $display("***TEST FAILED!");    
         err=1;
     end
+    
+    // 4. Rigorously test for direction again. (so the front part could be omitted - left in to show my learning process)
+    enable = 1;
+    #50
+    if (counter_out!=8'b00000101)       //going up by 5 steps
+    begin
+        $display("***TEST FAILED!");    
+        err=1;
+    end
+    direction = 0;
+    #30
+    if (counter_out!=8'b00000010)       //went down by 3 steps
+    begin
+        $display("***TEST FAILED!");    
+        err=1;
+    end
 end     // NOTE: this end is for the initial beginning at the start of this section
 
 //Finish simulation and check for success.
 initial begin
-#200                
+#110                
 if (err==0)	//let the above tests run for 2000ns. If no errors after that time, we take the code to be successful.
-$display("***TEST PASSED! :) ***");
+$display("TEST PASSED :D");
 $finish;
-
 end
  
-
 endmodule 
- 
