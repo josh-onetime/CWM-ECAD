53b553eeed9b0fa812b8e1e5aa47d460d6117f9a
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-03 10:59:47 +0000

    A complete solution to Ex6

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,22 +1,39 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Exercise #6 
-// Student Name:
-// Date: 
-//
-//
-//  Description: In this exercise, you need to design a multiplexer between a dice and traffic 
-//  lights, where the output acts according to the following truth table:
-//
-//  sel | out
-// -----------------
-//   0  | dice
-//   1  | traffic lights
-//
-//  inputs:
-//           rst, clk, button,sel
-//
-//  outputs:
-//           result[2:0]
-//
-//////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module dice_lights_multiplexer(
+	input rst,
+	input clk,
+	input button,
+	input sel,
+	output [2:0] result
+	);
+	
+//Wires
+	wire [2:0] result;
+	wire red;
+	wire amber;
+	wire green;
+	wire [2:0] throw;
+
+//Instantiate multiplex module
+    	mux multiplexer(
+    		.a (throw [2:0]),
+    		.b ({red,amber,green}),
+    		.sel (sel),
+     		.out (result [2:0])
+     		);
+
+//Instantiate dice module
+    	dice dies(
+		.button (button), 
+		.clk (clk), 	
+		.rst (rst),
+		.throw (throw[2:0]) );
+//Instantiate traffic_lights module
+	traffic_lights lights(
+		.clk (clk),
+		.red (red),
+		.amber (amber),
+		.green (green) );
+  
+endmodule
