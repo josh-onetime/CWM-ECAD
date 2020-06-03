3d711e7d012ffe9c2ed9dc6d473ca1709db23c8a
Cloud User <centos@ip-172-31-0-63.eu-west-2.compute.internal>
2020-06-03 15:50:52 +0000

    incomplete solution to Ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7
-// Student Name:
-// Date: 
+// Student Name: Joshua Gei
+// Date: 3/6/2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory.
@@ -10,7 +10,29 @@
 //           clk, a[2:0], b[2:0], read
 //
 //  outputs:
-//           result[4:0]
+//           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
 
+module multiplier(clk,a,b,read,result);
+
+// Ports
+	input clk,read;	        // input ports
+	input [2:0] a,b;    	// a and b are 3-bit inputs each representing a digit from 0 to 7.
+	output [5:0] result; 	// result is a 6-bit output representing a value from 0 to 49.
+	
+//Wires and registers
+	wire [5:0] address;        
+	
+//Logic.
+	assign address[5:0]=(a*8)+b;
+
+	memory_multiplier memory_multiplier_project(
+		.clka(clk),    		// input wire clka
+		.ena(read),      	// input wire ena
+		.addra(address),  	// input wire [5 : 0] addra
+		.douta(result)  	// output wire [5 : 0] douta
+	);
+ 
+endmodule
